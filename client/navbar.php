068e78411f31c828df847b536fc306f9679d607d
AB7zz <abhinavcv007@gmail.com>
2023-06-22 20:12:21 +0530

    navbar changes

--- client/navbar.php
+++ client/navbar.php
@@ -9,12 +9,20 @@
                         <li><a href="#">Help</a></li>
                     </ul>
                 </div>
-                <div>
+                <div id="not_logged">
                     <ul id="nav-links" class="nav-links">
                         <li><a href="#">english</a></li>
                         <li><a href="#">Products</a></li>
                         <li><a href="login.php">Log in</a></li>
                         <li><a href="signup.php" class="nav__cta">Sign up</a></li>
+                    </ul>
+                </div>
+                <div id="logged">
+                    <ul id="nav-links" class="nav-links">
+                        <li><a href="#">english</a></li>
+                        <li><a href="#">Products</a></li>
+                        <li><a href="profile.php" id="logged_name" class="nav__cta"></a></li>
+                        <li><a id="logged_side_btn">View Available Vehicles</a></li>
                     </ul>
                 </div>
                 <div class="hamburger">
@@ -24,3 +32,20 @@
             </nav>
         </div>
     </header>
+    <script>
+        if(localStorage.getItem('name')){
+            document.getElementById('not_logged').style.display = 'none'
+            document.getElementById('logged').style.display = 'block'
+            document.getElementById('logged_name').innerHTML = localStorage.getItem('name')
+            if(localStorage.getItem('type_id') == "1"){
+                document.getElementById('logged_side_btn').href = "driverDashboard.php"
+                document.getElementById('logged_side_btn').innerHTML = "Dashboard"
+            }else if(localStorage.getItem('type_id') == "2"){
+                document.getElementById('logged_side_btn').href = "availableVehicles.php"
+                document.getElementById('logged_side_btn').innerHTML = "View available vehicles"
+            }
+        }else{
+            document.getElementById('not_logged').style.display = 'block'
+            document.getElementById('logged').style.display = 'none'
+        }
+    </script>
